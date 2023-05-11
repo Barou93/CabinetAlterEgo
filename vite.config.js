6822e6d26bc652d1fca8affae1811e458da75b47
Oumar <oumarmauret@gmail.com>
2023-05-11 15:33:33 +0000

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@
 
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import viteImagemin from "vite-plugin-imagemin";
 
 export default defineConfig({
   build: {
@@ -25,15 +26,24 @@ export default defineConfig({
 
   plugins: [
     react(),
-    {
-      name: "compress-image",
-      transform: (code, id) => {
-        if (/\.(jpe?g|png|gif|svg)$/i.test(id)) {
-          return code.replace(/import\s.*from\s.*\;/g, ""); // Supprime les imports inutiles pour les images.
-        }
-        return code;
+    viteImagemin({
+      // Options de configuration pour vite-plugin-imagemin
+      gifsicle: {
+        optimizationLevel: 3,
       },
-    },
+      mozjpeg: {
+        quality: 80,
+      },
+      optipng: {
+        optimizationLevel: 5,
+      },
+      pngquant: {
+        quality: [0.6, 0.8],
+      },
+      svgo: {
+        plugins: [{ removeViewBox: false }, { cleanupIDs: false }],
+      },
+    }),
   ],
   server: {
     port: 3000,
